0dd648a71283b464b213ec21bbf1f75fe237372d
Victor Trajano <victor.silva@koper.com.br>
2025-12-02 13:46:11 -0300

    feat: funcionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { filter } from "rxjs";
+import { FipeService } from "./services/fipe.service";
+import { VehicleResponse, VehiclePriceHistory, VehicleRequestForm } from "./types/vehicle";
+import { VehicleTypeEnum } from "./types/vehicle-type.enum";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,105 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'fipe-search';
+  public brands: VehicleResponse[] = [];
+  public models: VehicleResponse[] = [];
+  public vehiclePriceHistory: VehiclePriceHistory[] = [];
+  public formGroup: FormGroup<VehicleRequestForm>;
+
+  public readonly vehicleTypeOptions = [
+    { code: VehicleTypeEnum.CARS, name: 'Carros e utilitários pequenos' },
+    { code: VehicleTypeEnum.MOTORCYCLES, name: 'Motos' },
+    { code: VehicleTypeEnum.TRUCKS, name: 'Caminhões e microônibus' },
+  ];
+
+  constructor(private fipeService: FipeService) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+    this.handleVehicleTypeChanges();
+    this.handleBrandChanges();
+    this.handleModelChanges();
+  }
+
+  public submitForm(): void {
+    if (this.formGroup.valid) {
+      const { vehicleType, brandId, modelId } = this.formGroup.getRawValue();
+    } else {
+      this.formGroup.markAllAsTouched();
+    }
+  }
+
+  public clearForm(): void {
+    this.formGroup.reset();
+  }
+
+  private initializeForm(): void {
+    this.formGroup = new FormGroup<VehicleRequestForm>({
+      vehicleType: new FormControl<VehicleTypeEnum | null>(
+        null,
+        Validators.required
+      ),
+      brandId: new FormControl<string | null>(
+        { value: null, disabled: true },
+        Validators.required
+      ),
+      modelId: new FormControl<string | null>(
+        { value: null, disabled: true },
+        Validators.required
+      ),
+    });
+  }
+
+  private handleVehicleTypeChanges(): void {
+    this.formGroup.controls.vehicleType.valueChanges
+      .pipe(
+        filter(
+          (vehicleType): vehicleType is VehicleTypeEnum => vehicleType !== null
+        )
+      )
+      .subscribe((vehicleType) => {
+        this.formGroup.controls.brandId.reset({ value: null, disabled: false });
+        this.formGroup.controls.modelId.reset({ value: null, disabled: true });
+        this.models = [];
+        this.vehiclePriceHistory = [];
+
+        this.fipeService.listBrands(vehicleType).subscribe((brands) => {
+          this.brands = brands;
+        });
+      });
+  }
+
+  private handleBrandChanges(): void {
+    this.formGroup.controls.brandId.valueChanges
+      .pipe(filter((brandId): brandId is string => brandId !== null))
+      .subscribe((brandId) => {
+        this.formGroup.controls.modelId.reset({ value: null, disabled: false });
+        this.vehiclePriceHistory = [];
+
+        const vehicleType = this.formGroup.controls.vehicleType.value;
+        if (!vehicleType) return;
+
+        this.fipeService
+          .listModels(vehicleType, brandId)
+          .subscribe((models) => {
+            this.models = models;
+          });
+      });
+  }
+
+  private handleModelChanges(): void {
+    this.formGroup.controls.modelId.valueChanges
+      .pipe(filter((modelId): modelId is string => modelId !== null))
+      .subscribe((modelId) => {
+        const vehicleType = this.formGroup.controls.vehicleType.value;
+        const brandId = this.formGroup.controls.brandId.value;
+        if (!vehicleType || !brandId) return;
+
+        this.fipeService
+          .getVehicleHistory(vehicleType, brandId, modelId)
+          .subscribe((years) => {
+            this.vehiclePriceHistory = years ?? [];
+          });
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { DividerModule } from 'primeng/divider';
@@ -9,12 +12,8 @@ import { InputTextModule } from 'primeng/inputtext';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
-  declarations: [AppComponent, VehicleFormComponent],
+  declarations: [AppComponent],
   imports: [
     CommonModule,
     BrowserModule,
