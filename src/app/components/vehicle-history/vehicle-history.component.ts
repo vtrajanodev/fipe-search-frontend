559e8ebab077e2d0981541f436640882b11d181d
Victor Trajano <victor.silva@koper.com.br>
2025-12-02 16:48:39 -0300

    feat

--- src/app/components/vehicle-history/vehicle-history.component.ts
+++ src/app/components/vehicle-history/vehicle-history.component.ts
@@ -12,4 +12,23 @@ export class VehicleHistoryComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  get historyDescription(): string {
+    console.log(this.vehicleHistory);
+
+    if (!this.vehicleHistory) {
+      return '';
+    }
+
+    const { year, price, diff, diffPercentage, previousYear } =
+      this.vehicleHistory;
+
+    let description = `Valor em ${year} -> ${price}`;
+
+    if (diff != null && diffPercentage != null && previousYear) {
+      description += `, alteração de R$ ${diff} (${diffPercentage}) em relação a ${previousYear}`;
+    }
+
+    return description;
+  }
 }
